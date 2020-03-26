a137dbed32d2c93446fd6219530e0bc2588b3939
German Cruz <cruzgerman216@yahoo.com>
2020-03-26 05:47:44 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,38 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    arr = []
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+     arr = doc.css(".roster-cards-container").css("a .card-text-container .student-name")
+     arr2 = doc.css(".roster-cards-container").css("a .card-text-container .student-location")
+
+     i = 0
+     getarr = []
+     while i < arr.length
+       name = arr[i].text.split(" ")
+        name[0] = name[0].downcase
+        name[1] =  name[1].to_s.downcase
+
+       url = "students/" + name.join("-") + ".html"
+       obj = {:name => arr[i].text, :location => arr2[i].text, :profile_url=>url}
+       getarr << obj
+       i += 1
+     end
+      getarr
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    html = open(profile_url)
+    doc = Nokogiri::HTML(html)
+    arr = doc.css(".vitals-container .social-icon-container a href").first
+    puts arr
   end
-
 end
 
+
+  Scraper.scrape_profile_page("https://learn-co-curriculum.github.io/student-scraper-test-page/students/ryan-johnson.html")
