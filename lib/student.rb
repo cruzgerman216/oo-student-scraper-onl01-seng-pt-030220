33e85f0d570d976d7d055c3f7b99c0c3b95abce8
German Cruz <cruzgerman216@yahoo.com>
2020-03-26 06:47:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,12 +17,9 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    @twitter = attributes_hash[:twitter]
-    @linkedin = attributes_hash[:linkedin]
-    @github = attributes_hash[:github]
-    @blog = attributes_hash[:blog]
-    @profile_quote = attributes_hash[:profile_quote]
-    @bio = attributes_hash[:bio]
+    attributes_hash.each do |k, info|
+        self.send(("#{k}="), info)
+      end
   end
 
   def self.all
